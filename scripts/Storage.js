b94e5061f52754fa9258c18a3a04c7ece5e5d896
Cruddle <up202107694@up.pt>
2024-07-10 00:12:46 +0100

    Firefox Version

--- scripts/Storage.js
+++ scripts/Storage.js
@@ -8,11 +8,7 @@ function setConfig(config){
 async function getConfig(){ 
     return new Promise((resolve, reject) => {
         chrome.storage.local.get({Shorts: []}, (result) => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-            } else {
                 resolve(result.Shorts);
-            }
         });
     });
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -112,11 +112,7 @@ function getViewAsVideoStatus(){
 async function getConfigFromStorage() { 
     return new Promise((resolve, reject) => {
         chrome.storage.local.get({Shorts: []}, (result) => {
-            if (chrome.runtime.lastError) {
                 reject(chrome.runtime.lastError);
-            } else {
-                resolve(result.Shorts);
-            }
         });
     });
 }
